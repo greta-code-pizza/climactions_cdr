7b0af67459529dd84b4db737fa8040f229617aef
cdbschr <c.debusscher@gmail.com>
2022-06-09 13:40:42 +0200

    👌 IMPROVE: readEmail > syntax

--- app/Views/admin/readEmail.php
+++ app/Views/admin/readEmail.php
@@ -13,7 +13,7 @@
         <li><?= $readEmail["object"]?></li>
         <li><?= $readEmail["message"]?></li>
         <li>Reçu le : <?= $readEmail["date"]?></li>
-        <li>Lu par : <?= $readEmail["prenom"]?> <?= $readEmail["nom"]?></li>
+        <li>Ouvert par : <?= $readEmail["prenom"]?> <?= $readEmail["nom"]?></li>
         <li>
             <span class="btn"><a href="indexAdmin.php?action=emailAdmin"><i class="fa-solid fa-arrow-left"></i></a></span>
             <span class="btn"><a class="delete" href="indexAdmin.php?action=deleteEmail&id=<?= $readEmail['id'] ?>"><i class="fa-solid fa-trash-can"></i></a></span>
